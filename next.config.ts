bb4d861b8cf752ead37ba1ccfb907852e29a5eca
Ameer Basha Sk <haneefsk921@gmail.com>
2025-09-04 01:57:28 +0000

    Credibility Report The results of the news analysis will be displayed he

--- next.config.ts
+++ next.config.ts
@@ -19,7 +19,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  experimental: {},
+  experimental: {
+    allowedDevOrigins: ["*.cloudworkstations.dev"],
+  },
 };
 
 export default nextConfig;
